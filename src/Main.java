f085e986d43ccde2a02a528db1462746affab2b7
TTranchese <tommaso.tranchese.96@gmail.com>
2023-02-23 12:46:55 +0100

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 	public static void main(String[] args) {
-		String string = "tup tuup tuuup tuuuup";
-		string = string.replaceAll("(uu|uuu)(?!u)", "x");
-		System.out.println(string);
+		String input = "tup tuup tuuup tuuuup";
+		//definisco il pattern e il matcher da usare sull'input, la regex l'ho cambiata a qualcosa di più semplice rispetto a quella di prima
+		Pattern pattern = Pattern.compile("u{2,3}");
+		Matcher matcher = pattern.matcher(input);
+		//definisco e assegno la variabile result utilizzando .replaceAll("x")
+		String result = matcher.replaceAll("x");
+		//stampo la variabile result
+		System.out.println(result);
 	}
 }
